9670fc9dd75e8ad591e35c05e7d1ba92bfa6078e
twlee <lteawoo@naver.com>
2021-02-17 21:08:48 +0900

    fs

--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
+import fs from 'fs'
 import express from 'express'
 import { Ppomppu } from './targets/ppomppu'
 
@@ -5,9 +6,22 @@ const app = express()
 
 export default app
 
+const BASE_DIR = 'C:/file'
+const PPOMPPU_FILE = 'ppomppu.json'
+
+const saveJson = (dealList) => {
+  fs.writeFile(`${BASE_DIR}/${PPOMPPU_FILE}`, JSON.stringify(dealList), (err) => {
+    if (err) {
+      return err
+    }
+  })
+}
+
 app.get('/', async (req, res, next) => {
   const ppomppu = new Ppomppu()
   const data = await ppomppu.parse()
 
-  res.send(data)
+  saveJson(data)
+
+  res.send()
 })
