b5b97cfc5172bb11a3cc924923a84286d74a9dc2
twlee <lteawoo@naver.com>
2021-03-16 21:27:04 +0900

    menu

--- layouts/default.vue
+++ layouts/default.vue
@@ -97,16 +97,6 @@ export default {
       fixed: false,
       items: [
         {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        },
-        {
           icon: 'mdi-information',
           title: 'Info',
           to: '/info'
